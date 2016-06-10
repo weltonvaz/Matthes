0470dccdbf2e03660902e1871f3a9f15083e6f68
Welton Vaz <weltonvaz@gmail.com>
2016-06-10 14:41:33 -0300

    Primeiro Comitação deste repositório

---
+++ dog.py
@@ -0,0 +1,14 @@
+class dog(object):
+    """ Uma tentativa simples de modelar um cachorro. """
+    def __init__(self, name, age):
+        """ Incializa os atributos name e age """
+        self.name = name
+        self.age = age
+
+    def sit():
+        """ Simula um cachorro sentado em resposta a um comando """
+        print(self.name.title() + " is now sitting.")
+
+    def roll_over(self):
+        """ Simula um cachorro rolandp em resposta a um comando."""
+        print(self.name.title() + " rolled over")
